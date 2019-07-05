c334b06fce754d2d39b5fa5f75992ca638866eac
daniel vicario <danigv.email@gmail.com>
2019-07-05 07:51:40 +0200

    first commit

---
+++ passport/index.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+
+require('./serializers');
+require('./localStrategy');
+
+module.exports = (app)  => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(function (req, res, next) {
+    res.locals.usuario = req.user
+    next()
+  })
+}
